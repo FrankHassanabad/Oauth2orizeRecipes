ecc5f4a657454f46429e217368fca7ec19b3fa8b
Frank Hassanabad <foreverlinux@yahoo.com>
2013-09-15 01:50:21 -0600

    Added more unit testing

---
+++ authorization-server/test/common/index.js
@@ -0,0 +1,4 @@
+exports.properties = require('./properties').properties;
+exports.request = require('./request').request;
+exports.validate = require('./validate').validate;
+
--- authorization-server/test/grant_type_code.js
+++ authorization-server/test/grant_type_code.js
@@ -1,6 +1,10 @@
 var assert = require("assert")
     , app = require("../app.js")
-    , request = require('request');
+    , request = require('request')
+    , helper = require('./common').request
+    , validate = require('./common').validate
+    , properties = require('./common').properties
+
 
 //Enable cookies so that we can perform logging in correctly to the OAuth server
 //and turn off the strict SSL requirement
@@ -11,48 +15,46 @@ var request = request.defaults({jar: true, strictSSL: false});
  * This follows the testing guide roughly from
  * https://github.com/FrankHassanabad/Oauth2orizeRecipes/wiki/OAuth2orize-Authorization-Server-Tests
  */
-describe('Grant Types', function () {
+describe('Grant Type Authorization Code', function () {
     //set the time out to be 20 seconds
     this.timeout(20000);
-    describe('Grant Type of authorization_code', function () {
+    describe('', function () {
         it('should work with the authorization_code asking for a refresh token', function (done) {
             //Log into the OAuth2 server as bob
-            request.post(
-                'https://localhost:3000/login', {
-                    form: {
-                        username: 'bob',
-                        password: 'secret'
-                    }
-                },
+            helper.login(
                 function (error, response, body) {
                     //Get the OAuth2 authorization code
-                    request.get(
-                        'https://localhost:3000/dialog/authorize?redirect_uri=https://localhost:3000&response_type=code&client_id=trustedClient&scope=offline_access',
+                    helper.getAuthorization({scope: 'offline_access'},
                         function (error, response, body) {
                             //Assert that we have the ?code in our URL
                             assert.equal(0, response.req.path.indexOf("/?code="));
                             var code = response.req.path.slice(7, response.req.path.length);
                             //Get the token
-                            request.post(
-                                'https://localhost:3000/oauth/token',
-                                {
-                                    form: {
-                                        code: code,
-                                        redirect_uri: 'https://localhost:3000',
-                                        client_id: 'trustedClient',
-                                        client_secret: 'ssh-otherpassword',
-                                        grant_type: 'authorization_code'
-                                    }
-                                },
+                            helper.postOAuthCode(code,
                                 function (error, response, body) {
-                                    //Assert that we got back an application/json response
-                                    assert.equal(response.headers["content-type"], "application/json");
-                                    var jsonResponse = JSON.parse(body);
-                                    assert.equal(Object.keys(jsonResponse).length, 4);
-                                    assert.equal(jsonResponse.access_token.length, 256);
-                                    assert.equal(jsonResponse.refresh_token.length, 256);
-                                    assert.equal(jsonResponse.expires_in, 3600);
-                                    assert.equal(jsonResponse.token_type, "bearer");
+                                    validate.validateAccessRefreshToken(response.headers, body);
+                                    var tokens = JSON.parse(body);
+                                    //Get the user info
+                                    helper.getUserInfo(tokens.access_token,
+                                        function(error, response, body) {
+                                            validate.validateUserJson(response.headers, body);
+                                        }
+                                    );
+                                    //Get another valid access token from the refresh token
+                                    helper.postRefeshToken(tokens.refresh_token, function(error, response, body) {
+                                        validate.validateAccessToken(response.headers, body);
+                                    });
+                                    //Get another valid access token from the refresh token
+                                    helper.postRefeshToken(tokens.refresh_token, function(error, response, body) {
+                                        validate.validateAccessToken(response.headers, body);
+                                    });
+                                }
+                            );
+                            //Try to get the token again but we shouldn't be able to reuse the same code
+                            helper.postOAuthCode(code,
+                                function (error, response, body) {
+                                    assert.equal(response.statusCode, 400);
+                                    validate.validateInvalidCodeError(response.headers, body);
                                     done();
                                 }
                             );
@@ -61,43 +63,26 @@ describe('Grant Types', function () {
                 }
             );
         });
-        it('should work with the authorization_code asking not asking for a refresh token', function (done) {
+        it('should work with the authorization_code not asking for a refresh token', function (done) {
             //Log into the OAuth2 server as bob
-            request.post(
-                'https://localhost:3000/login', {
-                    form: {
-                        username: 'bob',
-                        password: 'secret'
-                    }
-                },
+            helper.login(
                 function (error, response, body) {
                     //Get the OAuth2 authorization code
-                    request.get(
-                        'https://localhost:3000/dialog/authorize?redirect_uri=https://localhost:3000&response_type=code&client_id=trustedClient',
+                    helper.getAuthorization({},
                         function (error, response, body) {
                             //Assert that we have the ?code in our URL
                             assert.equal(0, response.req.path.indexOf("/?code="));
                             var code = response.req.path.slice(7, response.req.path.length);
                             //Get the token
-                            request.post(
-                                'https://localhost:3000/oauth/token',
-                                {
-                                    form: {
-                                        code: code,
-                                        redirect_uri: 'https://localhost:3000',
-                                        client_id: 'trustedClient',
-                                        client_secret: 'ssh-otherpassword',
-                                        grant_type: 'authorization_code'
-                                    }
-                                },
+                            helper.postOAuthCode(code,
                                 function (error, response, body) {
-                                    //Assert that we got back an application/json response
-                                    assert.equal(response.headers["content-type"], "application/json");
-                                    var jsonResponse = JSON.parse(body);
-                                    assert.equal(Object.keys(jsonResponse).length, 3);
-                                    assert.equal(jsonResponse.access_token.length, 256);
-                                    assert.equal(jsonResponse.expires_in, 3600);
-                                    assert.equal(jsonResponse.token_type, "bearer");
+                                    validate.validateAccessToken(response.headers, body);
+                                    //Get the user info
+                                    helper.getUserInfo(JSON.parse(body).access_token,
+                                        function(error, response, body) {
+                                            validate.validateUserJson(response.headers, body);
+                                        }
+                                    );
                                     done();
                                 }
                             );
@@ -106,19 +91,11 @@ describe('Grant Types', function () {
                 }
             );
         });
-        it('should work with the authorization_code with an invalid client id', function (done) {
-            //Log into the OAuth2 server as bob
-            request.post(
-                'https://localhost:3000/login', {
-                    form: {
-                        username: 'bob',
-                        password: 'secret'
-                    }
-                },
+        it('should give an error with an invalid client id', function (done) {
+            helper.login(
                 function (error, response, body) {
                     //Get the OAuth2 authorization code
-                    request.get(
-                        'https://localhost:3000/dialog/authorize?redirect_uri=https://localhost:3000&response_type=code&client_id=someInvalidClientId',
+                    helper.getAuthorization({clientId: 'someinvalidclientid'},
                         function (error, response, body) {
                             //assert that we are getting an error code of 400
                             assert.equal(response.statusCode, 400);
@@ -128,19 +105,12 @@ describe('Grant Types', function () {
                 }
             );
         });
-        it('should work with the authorization_code with a missing client id', function (done) {
-            //Log into the OAuth2 server as bob
-            request.post(
-                'https://localhost:3000/login', {
-                    form: {
-                        username: 'bob',
-                        password: 'secret'
-                    }
-                },
+        it('should give an error with a missing client id', function (done) {
+            helper.login(
                 function (error, response, body) {
                     //Get the OAuth2 authorization code
                     request.get(
-                        'https://localhost:3000/dialog/authorize?redirect_uri=https://localhost:3000&response_type=code',
+                        properties.authorization + '?redirect_uri=' + properties.redirect + "&response_type=code",
                         function (error, response, body) {
                             //assert that we are getting an error code of 400
                             assert.equal(response.statusCode, 400);
@@ -150,19 +120,11 @@ describe('Grant Types', function () {
                 }
             );
         });
-        it('should work with an invalid response type', function (done) {
-            //Log into the OAuth2 server as bob
-            request.post(
-                'https://localhost:3000/login', {
-                    form: {
-                        username: 'bob',
-                        password: 'secret'
-                    }
-                },
+        it('should give an error with an invalid response type', function (done) {
+            helper.login(
                 function (error, response, body) {
                     //Get the OAuth2 authorization code
-                    request.get(
-                        'https://localhost:3000/dialog/authorize?redirect_uri=https://localhost:3000&response_type=invalid&client_id=trustedClient',
+                    helper.getAuthorization({responseType: 'invalid'},
                         function (error, response, body) {
                             //assert that we are getting an error code of 400
                             assert.equal(response.statusCode, 400);
