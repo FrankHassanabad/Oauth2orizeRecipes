dd4da94f6916de43c519d2ad3811988340dc08cc
Frank Hassanabad <foreverlinux@yahoo.com>
2013-09-15 23:03:50 -0600

    docs update

--- authorization-server/db/refreshtokens.js
+++ authorization-server/db/refreshtokens.js
@@ -1,18 +1,44 @@
-//TODO Document all of this
+//The access token and optionally refresh token.
+//You will use these to access your end point data through the means outlined
+//in the RFC The OAuth 2.0 Authorization Framework: Bearer Token Usage
+//(http://tools.ietf.org/html/rfc6750)
 
+/**
+ * Tokens in-memory data structure which stores all of the refresh tokens
+ */
 var tokens = {};
 
-
+/**
+ * Returns a refresh token if it finds one, otherwise returns
+ * null if one is not found.
+ * @param key The key to the refresh token
+ * @param done The function to call next
+ * @returns The refresh token if found, otherwise returns null
+ */
 exports.find = function(key, done) {
     var token = tokens[key];
     return done(null, token);
 };
 
+/**
+ * Saves a refresh token, user id, client id, and scope.
+ * @param token The refresh token (required)
+ * @param userID The user ID (required)
+ * @param clientID The client ID (required)
+ * @param scope The scope (optional)
+ * @param done Calls this with null always
+ * @param returns this with null
+ */
 exports.save = function(token, userID, clientID, scope, done) {
     tokens[token] = { userID: userID, clientID: clientID, scope: scope};
     return done(null);
 };
 
+/**
+ * Deletes a refresh token
+ * @param key The refresh token to delete
+ * @param done returns this when done
+ */
 exports.delete = function(key, done) {
     delete tokens[key];
     return done(null);
