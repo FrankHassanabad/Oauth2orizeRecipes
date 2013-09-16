f9e055686adc27ed1dacca2568485eb6045b6186
Frank Hassanabad <foreverlinux@yahoo.com>
2013-09-15 23:01:37 -0600

    docs update

--- authorization-server/db/clients.js
+++ authorization-server/db/clients.js
@@ -1,5 +1,19 @@
-//TODO Document all of this
-
+/**
+ * This is the configuration of the clients that are allowed to connected to your authorization server.
+ * These represent client applications that can connect.  At a minimum you need the required properties of
+ *
+ * id: (A unique numeric id of your client application )
+ * name: (The name of your client application)
+ * clientId: (A unique id of your client application
+ * clientSecret: (A unique password(ish) secret that is _best not_ shared with anyone but your client
+ *     application and the authorization server.
+ *
+ * Optionally you can set these properties which are
+ * trustedClient: (default if missing is false.  If this is set to true then the client is regarded as a
+ *     trusted client and not a 3rd party application.  That means that the user will not be presented with
+ *     a decision dialog with the trusted application and that the trusted application gets full scope access
+ *     without the user having to make a decision to allow or disallow the scope access.
+ */
 var clients = [
     {
         id: '1',
@@ -22,7 +36,13 @@ var clients = [
     }
 ];
 
-
+/**
+ * Returns a client if it finds one, otherwise returns
+ * null if a client is not found.
+ * @param id The unique id of the client to find
+ * @param done The function to call next
+ * @returns The client if found, otherwise returns null
+ */
 exports.find = function(id, done) {
   for (var i = 0, len = clients.length; i < len; i++) {
     var client = clients[i];
@@ -33,6 +53,13 @@ exports.find = function(id, done) {
   return done(null, null);
 };
 
+/**
+ * Returns a client if it finds one, otherwise returns
+ * null if a client is not found.
+ * @param clientId The unique client id of the client to find
+ * @param done The function to call next
+ * @returns The client if found, otherwise returns null
+ */
 exports.findByClientId = function(clientId, done) {
   for (var i = 0, len = clients.length; i < len; i++) {
     var client = clients[i];
