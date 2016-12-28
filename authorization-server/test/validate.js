304435b7d62a53afaa6680aab760259256d8c1d4
Frank Hassanabad <FrankHassanbad@gmail.com>
2016-12-28 13:28:35 -0700

    Fixed simple wrong wording with validate unit test and added build badge to the project

--- authorization-server/test/validate.js
+++ authorization-server/test/validate.js
@@ -10,7 +10,7 @@ chai.use(sinonChai);
 const expect = chai.expect;
 
 
-describe('accesstokens', () => {
+describe('validate', () => {
   describe('#logAndThrow', () => {
     it('should throw a given mesage', () => {
       expect(() => validate.logAndThrow('some message')).to.throw('some message');
