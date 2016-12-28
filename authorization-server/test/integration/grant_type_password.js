ff277fb34ff0cd79bf41027a4530a4cda23ea50c
Frank Hassanabad <FrankHassanbad@gmail.com>
2016-12-28 08:41:15 -0700

    Broke out integration tests  * Moved integration tests into integration folder  * Made test:integration script for running them separate  * ES6 promisified the integration tests

---
+++ authorization-server/test/integration/grant_type_password.js
@@ -0,0 +1,42 @@
+'use strict';
+
+// This is needed to start the server for the tests since these are more integration than unit tests
+require('../../app.js');
+
+const helper   = require('./common').helper;
+const validate = require('./common').validate;
+
+/**
+ * Tests for the Grant Type of Password.
+ * This follows the testing guide roughly from
+ * https://github.com/FrankHassanabad/Oauth2orizeRecipes/wiki/OAuth2orize-Authorization-Server-Tests
+ */
+describe('Grant Type Password', () => {
+  it('should work with asking for an access token and refresh token', () =>
+    helper.postOAuthPassword('offline_access')
+    .then(([response, body]) => {
+      validate.validateAccessRefreshToken(response, body);
+      return JSON.parse(body);
+    })
+    .then((tokens) => {
+      const userInfo = helper.getUserInfo(tokens.access_token)
+      .then(([response, body]) => validate.validateUserJson(response, body));
+
+      const refreshToken = helper.postRefeshToken(tokens.refresh_token)
+      .then(([response, body]) => validate.validateAccessToken(response, body));
+
+      const refreshToken2 = helper.postRefeshToken(tokens.refresh_token)
+      .then(([response, body]) => validate.validateAccessToken(response, body));
+
+      return Promise.all([userInfo, refreshToken, refreshToken2]);
+    }));
+
+  it('should work just an access token and a scope of undefined', () =>
+    helper.postOAuthPassword(undefined)
+    .then(([response, body]) => {
+      validate.validateAccessToken(response, body);
+      return JSON.parse(body);
+    })
+    .then(tokens => helper.getUserInfo(tokens.access_token))
+    .then(([response, body]) => validate.validateUserJson(response, body)));
+});
