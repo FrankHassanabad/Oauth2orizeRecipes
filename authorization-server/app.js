7804f45ebd850df10d637555fabc9455368d077e
Frank Hassanabad <FrankHassanbad@gmail.com>
2016-12-30 21:50:52 -0700

    Updated test certificates, updated from 1024 to 2048 since I would never really use 1024 and it is a bad example for others

--- authorization-server/app.js
+++ authorization-server/app.js
@@ -89,7 +89,7 @@ setInterval(() => {
 }, config.db.timeToCheckExpiredTokens * 1000);
 
 // TODO: Change these for your own certificates.  This was generated through the commands:
-// openssl genrsa -out privatekey.pem 1024
+// openssl genrsa -out privatekey.pem 2048
 // openssl req -new -key privatekey.pem -out certrequest.csr
 // openssl x509 -req -in certrequest.csr -signkey privatekey.pem -out certificate.pem
 const options = {
