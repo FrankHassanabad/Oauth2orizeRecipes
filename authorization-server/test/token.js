6fa36a3ee246dde219a92fd551600e29d2a172e6
Frank Hassanabad <FrankHassanbad@gmail.com>
2017-01-01 17:38:13 -0700

    Added revocation endpoint that is similar to google revocation  * You can revoke access tokens  * You can revoke refresh tokens  * Added unit tests for the revocation endpoint

--- authorization-server/test/token.js
+++ authorization-server/test/token.js
@@ -2,12 +2,11 @@
 
 require('process').env.OAUTHRECIPES_SURPRESS_TRACE = true;
 
-
-const chai             = require('chai');
-const sinonChai        = require('sinon-chai');
-const { accessTokens } = require('../db');
-const utils            = require('../utils');
-const token            = require('../token');
+const chai                            = require('chai');
+const sinonChai                       = require('sinon-chai');
+const { accessTokens, refreshTokens } = require('../db');
+const utils                           = require('../utils');
+const token                           = require('../token');
 
 chai.use(sinonChai);
 const expect = chai.expect;
@@ -84,6 +83,85 @@ describe('token', () => {
       const createdToken = utils.createToken();
       return token.info({
         query : { access_token : createdToken },
+      }, {
+        status : (statusCode) => {
+          expect(statusCode).to.eql(400);
+        },
+        json : (message) => {
+          expect(message).eql({ error: 'invalid_token' });
+        },
+      });
+    });
+  });
+
+  describe('#revoke', () => {
+    it('should throw an invalid http 400 on null', () =>
+      token.revoke({
+        query : { token : null },
+      }, {
+        status : (statusCode) => {
+          expect(statusCode).to.eql(400);
+        },
+        json : (message) => {
+          expect(message).eql({ error: 'invalid_token' });
+        },
+      }));
+
+    it('should throw an invalid http 400 on an undefined access token', () =>
+      token.revoke({
+        query : { },
+      }, {
+        status : (statusCode) => {
+          expect(statusCode).to.eql(400);
+        },
+        json : (message) => {
+          expect(message).eql({ error: 'invalid_token' });
+        },
+      }));
+
+    it('should throw an invalid http 400 on a bunk access token', () =>
+      token.revoke({
+        query : { token : 'abc' },
+      }, {
+        status : (statusCode) => {
+          expect(statusCode).to.eql(400);
+        },
+        json : (message) => {
+          expect(message).eql({ error: 'invalid_token' });
+        },
+      }));
+
+    it('should work with a valid access token', () => {
+      const createdToken = utils.createToken();
+      accessTokens.save(createdToken, new Date(0), '1', '1', '*');
+      return token.revoke({
+        query : { token : createdToken },
+      }, {
+        json : (message) => {
+          expect(message).eql({});
+        },
+      });
+    });
+
+    it('should work with a valid refresh token token', () => {
+      const createdToken = utils.createToken();
+      refreshTokens.save(createdToken, '1', '1', '*');
+      return token.revoke({
+        query : { token : createdToken },
+      }, {
+        status : (statusCode) => {
+          expect(statusCode).to.eql(200);
+        },
+        json : (message) => {
+          expect(message).eql({});
+        },
+      });
+    });
+
+    it('should throw an invalid http 400 when no saved token exists', () => {
+      const createdToken = utils.createToken();
+      return token.revoke({
+        query : { token : createdToken },
       }, {
         status : (statusCode) => {
           expect(statusCode).to.eql(400);
